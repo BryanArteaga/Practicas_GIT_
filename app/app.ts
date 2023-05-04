e52983b4c396adbae548737af14706ff87840359
BryanArteaga <stevenbryan361@gmail.com>
2023-05-04 18:39:26 -0500

    pruebas repositorio 2

---
+++ app/app.ts
@@ -0,0 +1,15 @@
+console.log('hola mundo')
+
+document.write('hola mundo')
+
+//Operador Postfix 
+
+//ejemplo 1
+let i: number = 10;
+i++;
+console.log("incremental",+ i);
+
+//ejemplo 2
+let a: number = 10;
+a--;
+console.log("Descremental=", -a)
