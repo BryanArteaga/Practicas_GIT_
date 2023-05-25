e93729a8aee080e066a4f44720a5f2f9f60dbaf5
BryanArteaga <132154411+BryanArteaga@users.noreply.github.com>
2023-05-24 20:19:38 -0500

    Update app.ts

--- app/app.ts
+++ app/app.ts
@@ -1,20 +1,3 @@
-console.log('hola mundo')
-
-document.write('hola mundo')
-
-//Operador Postfix 
-
-//ejemplo 1
-let i: number = 10;
-i++;
-console.log("incremental",+ i);
-
-//promt
-let a: string = "hola mundo"
-let nombre = prompt('hola')
-prompt('hola') 
-var b = prompt
-
 //
 function lista(...datos: string[]): void{
     const dato1: string | null = prompt("Ingresar especies de animales:");
