2f28bf53c20f8ab5092b7d8ad1907ef3ad0a2a56
BryanArteaga <132154411+BryanArteaga@users.noreply.github.com>
2023-05-24 20:19:19 -0500

    Update app.ts

--- app/app.ts
+++ app/app.ts
@@ -14,3 +14,19 @@ let a: string = "hola mundo"
 let nombre = prompt('hola')
 prompt('hola') 
 var b = prompt
+
+//
+function lista(...datos: string[]): void{
+    const dato1: string | null = prompt("Ingresar especies de animales:");
+     if (dato1 !== null && dato1 !== "") {
+         datos.push(dato1);
+         lista(...datos);
+}else {
+    mostrar(datos);
+    }
+}
+function mostrar(datos: string[]): void {
+    for (let i = 0; i < datos.length; i++) {
+    document.write(datos[i]);
+  }
+}    
