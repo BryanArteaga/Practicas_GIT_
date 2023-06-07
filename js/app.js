0670f934cd4ad58b61338e73f17dc5588673daf6
BryanArteaga <132154411+BryanArteaga@users.noreply.github.com>
2023-06-06 22:53:17 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
 "use strict";
-console.log('hola mundo');
-document.write('hola mundo');
-//Operador Postfix 
-//ejemplo 1
-let i = 10;
-i++;
-console.log("incremental", +i);
-//ejemplo 2
-let a = 10;
-a--;
-console.log("Descremental=", -a);
