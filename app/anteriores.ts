bf88c72b528aef63ee3623f89f0e034e4598edac
BryanArteaga <132154411+BryanArteaga@users.noreply.github.com>
2023-06-14 20:03:53 -0500

    Update anteriores.ts

---
+++ app/anteriores.ts
@@ -0,0 +1,16 @@
+var data:any=[{
+    "id":1,
+    'nombre': 'beta',
+    'color':'Azul',
+},
+{
+    'id':2,
+    'años':'20',
+    'estatura':'180',
+    'ciudad':'manta',
+},
+{
+    'id':3,
+    
+}
+]
